e8ef333d916d5a6c341628ac86bdf843a4cec63c
Andrii Pindiura <aydnep@aydnep.com.ua>
2016-11-03 16:45:17 +0200

    init

---
+++ src/components/main/main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../img/logo.svg';
+import style from './main.css';
+
+const Main = () => {
+  console.log(style);
+  return (
+    <div className="ap-react-app main">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
+      </div>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+};
+
+export default Main;
